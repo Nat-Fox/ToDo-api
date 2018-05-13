4e6cdf58db0cf1413391b7c510a15f6655054fc2
Natalia Garrido <natgramirez@outlook.com>
2018-05-13 15:34:06 -0400

    adding readme and comment adding readme, and comment verbs put and delete

---
+++ README.md
@@ -0,0 +1,3 @@
+TODO APP
+
+* Server with ExpressJS
--- index.js
+++ index.js
@@ -9,13 +9,13 @@ app.post('/api/todos', function (req, res) {
   res.send('post todos');
 });
 
-app.put('/api/todos', function(req, res) {
-  res.send('put todos');
-});
+// app.put('/api/todos', function(req, res) {
+//   res.send('put todos');
+// });
 
-app.delete('/api/todos', function (req, res) {
-  res.send('delete todos');
-});
+// app.delete('/api/todos', function (req, res) {
+//   res.send('delete todos');
+// });
 
 app.listen(3000, function () {
   console.log('Todo App running in http://127.0.0.1:' + 3000);
