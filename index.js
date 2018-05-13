6da467bfa80e24d73df9819b508264afc7805763
Natalia Garrido <natgramirez@outlook.com>
2018-05-13 15:24:57 -0400

    adding http verbs and testing with postman

--- index.js
+++ index.js
@@ -1,11 +1,22 @@
 var express = require('express');
 var app = express();
 
-// respond with "hello world" when a GET request is made to the homepage
-app.get('/', function(req, res) {
-  res.send('hello world');
+app.get('/api/todos', function(req, res) {
+  res.send('get todos');
+});
+
+app.post('/api/todos', function (req, res) {
+  res.send('post todos');
+});
+
+app.put('/api/todos', function(req, res) {
+  res.send('put todos');
+});
+
+app.delete('/api/todos', function (req, res) {
+  res.send('delete todos');
 });
 
 app.listen(3000, function () {
-  console.log('Todo App running in localhost:' + 3000);
+  console.log('Todo App running in http://127.0.0.1:' + 3000);
 });
